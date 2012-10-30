ee68923fa2edd5b0455a6469a5891d409249c9bf
Hung Doan <hdoan741@gmail.com>
2012-10-31 00:21:14 +0800

    Setup for heroku.

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 
 var express = require('express');
 var app = express()
--- tmp.js
+++ tmp.js
@@ -1,7 +1,3 @@
-var a = 1;
-var b = 2;
-var c = 3;
-var d = [1, 2];
-var e = {'a': 1, 'b': 2};
-var f = [d, e];
-var g = {'x': f, 'y': e};
+var x = 5;
+var y = 10;
+var z = x + y;
